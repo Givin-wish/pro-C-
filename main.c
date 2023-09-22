879cad4b8564685647b7080629f20542a8797d77
Givin  Bahakoula <138108464+Givin-wish@users.noreply.github.com>
2023-09-22 17:14:36 +0200

    Add files via upload

---
+++ main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main() {
+    int PositionJoueur = 0;
+    int PositionArrivee = 100;
+    int DeplacementJoueur = 0;
+    int DeplacementAdversaire = 0;
+    char Joueur = 'A';
+    char Joueur[] = "Givin";
+    _Bool jeuTermine = 0;
+
+    struct joeur
+    {
+        char Nompersonnage[30];
+        int Agedujouer;
+    };
+    
+    struct equipe
+    {
+        char Nomequipe[50];
+        int couleurequipe;
+    };
+     
+    printf("Nompersonnage : %s\n", );
+    
+	return 0;
+}
